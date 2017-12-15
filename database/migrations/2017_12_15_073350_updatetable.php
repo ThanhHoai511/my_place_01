f8012678f13065154e8f2d2a923aeb5b1c1b5790
Kenini1805 <huusu1996@gmail.com>
2017-12-15 13:40:15 +0700

    create migration
    updatetableuser

---
+++ database/migrations/2017_12_15_073350_updatetable.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Updatetable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('avatar');
+            $table->string('add');
+            $table->string('phone');
+            $table->string('dateofbirth');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
