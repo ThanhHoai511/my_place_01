7fb3b88d38f210c03792d24d34bb6a3b6af53377
BunnyPi04 <hangpt248@gmail.com>
2017-12-22 13:02:15 +0700

    add backend view

--- config/asset.php
+++ config/asset.php
@@ -2,5 +2,6 @@
 return [
     'image_path' => [
         'icon' => 'images/icon/',
+        'slide' => 'images/slide/'
     ],
 ];
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,5 @@
 */
 
 Route::get('/', function () {
-    return view('backend.master');
+    return view('welcome');
 });
